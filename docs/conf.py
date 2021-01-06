8805742220acd278145db90e11577538cd3273e8
janus <janus@insignificancegalore.net>
2021-01-06 15:54:24 +0100

    Remove sphinx-autodoc-typehints extension
    Feature in internal from sphinx 3.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,75 +1,17 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
+
+# -- Project information -----------------------------------------------------
+project = 'my project'
+copyright = '2019, Janus'
+author = 'Janus'
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../'))
-
-
-# -- Project information -----------------------------------------------------
+# Rely on editable install for establishing packages on path
 import os
 
-project = 'sphinx test'
-copyright = '2019, NCC'
-author = 'NCC'
-
-
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
-# Default role (to be used for `foo`) is :any:
-# https://www.sphinx-doc.org/en/master/usage/restructuredtext/roles.html#role-any
-default_role = "any"
-
-extensions = [
-    'sphinx.ext.autodoc',      # generate API documentation by scanning code
-    'sphinx.ext.napoleon',     # allow google-style docstrings
-    'sphinxcontrib.plantuml',  # allow embedded plantuml UML diagrams. Has dependencies
-    'sphinx.ext.graphviz',     # allow embedded graphviz figures. Requires graphviz on path
-    'recommonmark',             # Allow markdown files
-    'sphinx.ext.mathjax',       # allow latex math input
-    'sphinx_autodoc_typehints', # must come after napoleon, https://github.com/agronholm/sphinx-autodoc-typehints
-]
-
-# Configure recommonmark
-# http://www.sphinx-doc.org/en/master/usage/markdown.html
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
-
-# Configure m2r -- not active as markdown parser has issues
-# 'm2r'  # Allow markdown within ReST files
-# https://github.com/miyakogi/m2r#sphinx-integration
-# source_suffix = ['.rst', '.md']
-
-# Configure napoleon docstring parser
-# http://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
-napoleon_numpy_docstring = False  # only allow google docstring
-napoleon_google_docstring = True
-
-
-# Configure sphinx_autodoc_typehints
-always_document_param_types = True
-
-# Configure plantuml
-# https://pypi.org/project/sphinxcontrib-plantuml/
-plantuml = os.getenv('PLANTUML')
-plantuml_output_format = 'svg_img'
-
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -79,17 +21,46 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Default role (to be used for `foo`) is :any:
+# https://www.sphinx-doc.org/en/master/usage/restructuredtext/roles.html#role-any
+default_role = "any"
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'default'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# -- Extensions --------------------------------------------------------------
 
+extensions = []
+
+# Configure recommonmark
+# http://www.sphinx-doc.org/en/master/usage/markdown.html
+extensions.append('recommonmark')  # Allow markdown files
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+# Configure plantuml. allow embedded plantuml UML diagrams. Has dependencies including graphviz
+# https://pypi.org/project/sphinxcontrib-plantuml/
+extensions.append('sphinxcontrib.plantuml')  # 
+extensions.append('sphinx.ext.graphviz')  # allow embedded graphviz figures. Requires graphviz on path
+plantuml = os.getenv('PLANTUML')
+plantuml_output_format = 'svg_img'
+
+# Allow mathjax for math
+extensions.append('sphinx.ext.mathjax')  # allow latex math input
+
+
+# Configure autodoc
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_typehints
+extensions.append('sphinx.ext.autodoc')  # generate API documentation by scanning code
+autodoc_typehints = 'description'  # new in sphinx 3.0
+
+# Configure napoleon docstring parser
+# http://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
+extensions.append('sphinx.ext.napoleon')  # allow google-style docstrings
+napoleon_numpy_docstring = False  # only allow google docstring
+napoleon_google_docstring = True
